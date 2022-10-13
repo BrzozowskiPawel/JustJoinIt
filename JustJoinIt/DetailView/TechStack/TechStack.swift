0ab12be9099134cc4cc20c440912533ecfad7ff7
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-10-13 21:58:54 +0200

    Added package

---
+++ JustJoinIt/DetailView/TechStack/TechStack.swift
@@ -0,0 +1,20 @@
+//
+//  TechStack.swift
+//  JustJoinIt
+//
+//  Created by Paweł Brzozowski on 13/10/2022.
+//
+
+import SwiftUI
+
+struct TechStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TechStack_Previews: PreviewProvider {
+    static var previews: some View {
+        TechStack()
+    }
+}
